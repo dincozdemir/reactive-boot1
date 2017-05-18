4b450a100a1d10da1ae65195abe0f3bc3681b4e5
dincozdemir <dincozdemir@gmail.com>
2017-05-19 02:23:44 +0300

    first commit

---
+++ src/main/java/com/dinco/reactiveboot/ReactiveBoot1Application.java
@@ -0,0 +1,12 @@
+package com.dinco.reactiveboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveBoot1Application.class, args);
+	}
+}
---
+++ src/test/java/com/dinco/reactiveboot/ReactiveBoot1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dinco.reactiveboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveBoot1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
