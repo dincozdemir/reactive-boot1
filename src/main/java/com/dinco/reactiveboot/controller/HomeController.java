b2afe6fcb867a94d35791b84193d7cbbae506bcf
dincozdemir <dincozdemir@gmail.com>
2017-05-19 20:34:06 +0300

    reactive working

--- src/main/java/com/dinco/reactiveboot/controller/HomeController.java
+++ src/main/java/com/dinco/reactiveboot/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.dinco.reactiveboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Dinco-WORK on 19.05.2017.
  */
-@RestController
+@Controller
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "index.html.test";
+        return "index";
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,20 +1,44 @@
+let moviesRequestStream = Rx.Observable.from(axios.get('/movies/'));
 
-if (!!window.EventSource) {
-    var source = new EventSource('/movies/542c12e6-35cd-4995-b2f7-53c712064615/events');
+moviesRequestStream.subscribe(({ data }) => {
+    // execute the request
+    console.log(data);
+    var responseStream = Rx.Observable.create(function (observer) {
+        const eventSource = new EventSource('/movies/' + data[0].id + '/events');
+        eventSource.onmessage = x => observer.next(x);
+        eventSource.onerror = x => observer.error(console.log('EventSource failed'));
+    });
+
+    responseStream.subscribe(function({ data }) {
+        // do something with the response
+        console.log(data);
+    });
+});
+
+
+
+if (false) {
+    let movie = '';
+    axios.get('/movies/')
+        .then(function ({data}) {
+            console.log(data);
+            movie = data[1];
+            var source = new EventSource('/movies/' + movie.id + '/events');
+
+            source.addEventListener('message', function(e) {
+                console.log(e.data);
+            }, false);
+
+            source.addEventListener('open', function(e) {
+                // Connection was opened.
+            }, false);
+
+            source.addEventListener('error', function(e) {
+                if (e.readyState == EventSource.CLOSED) {
+                    // Connection was closed.
+                }
+            }, false);
+        });
 } else {
     // Result to xhr polling :(
 }
-
-source.addEventListener('message', function(e) {
-    console.log(e.data);
-}, false);
-
-source.addEventListener('open', function(e) {
-    // Connection was opened.
-}, false);
-
-source.addEventListener('error', function(e) {
-    if (e.readyState == EventSource.CLOSED) {
-        // Connection was closed.
-    }
-}, false);
